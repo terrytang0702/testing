885047cfc95aa2fd774530f834e1d48c56bec58c
terrytang0702 <terry_tang@hiphing.com.hk>
2018-07-30 17:21:58 +0800

    update 6
    update 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { Pro } from '@ionic/pro';
-import { ErrorHandler, NgModule,Injectable, Injector } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
-Pro.init('0ebfe8e6', {
-  appVersion: '0.0.1'
-})
-
-@Injectable()
-export class MyErrorHandler implements ErrorHandler {
-  ionicErrorHandler: IonicErrorHandler;
-
-  constructor(injector: Injector) {
-    try {
-      this.ionicErrorHandler = injector.get(IonicErrorHandler);
-    } catch(e) {
-      // Unable to get the IonicErrorHandler provider, ensure
-      // IonicErrorHandler has been added to the providers list below
-    }
-  }
-
-  handleError(err: any): void {
-    Pro.monitoring.handleNewError(err);
-    // Remove this if you want to disable Ionic's auto exception handling
-    // in development mode.
-    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
-  }
-}
 @NgModule({
   declarations: [
     MyApp,
@@ -48,8 +24,7 @@ export class MyErrorHandler implements ErrorHandler {
   providers: [
     StatusBar,
     SplashScreen,
-    IonicErrorHandler,
-    [{ provide: ErrorHandler, useClass: MyErrorHandler }]
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
